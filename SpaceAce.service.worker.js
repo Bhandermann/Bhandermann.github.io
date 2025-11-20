ea5257973d7aff4e1df2ebe7ca148c9e8450cca1
Bhandermann <blazehandermann@gmail.com>
2025-11-20 14:02:12 -0800

    space ace

--- SpaceAce.service.worker.js
+++ SpaceAce.service.worker.js
@@ -4,9 +4,9 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1754179827|26564497';
+const CACHE_VERSION = '1763676097|1892790240';
 /** @type {string} */
-const CACHE_PREFIX = 'SpaceAce-sw-cache-';
+const CACHE_PREFIX = 'Elite Sniper-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 /** @type {string} */
 const OFFLINE_URL = 'SpaceAce.offline.html';
